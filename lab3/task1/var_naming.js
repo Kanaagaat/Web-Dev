05d6a138dd8b3f4dfb3f472a6d3b8c4489007abf
Kanagat <132980743+Kanaagaat@users.noreply.github.com>
2026-02-09 23:56:38 +0500

    add lab 4

---
+++ lab3/task1/var_naming.js
@@ -0,0 +1,14 @@
+let userName;
+let test123;
+
+let $ = 1; // declared a variable with the name "$"
+let _ = 2; // and now a variable with the name "_"
+
+alert($ + _); // 3
+
+let имя = '...';
+let 我 = '...';
+// Non-Latin letters are allowed, but not recommended
+
+// let let = 5; // can't name a variable "let", error!
+// let return = 5; // also can't name it "return", error!
